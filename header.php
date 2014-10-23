5146b7559b32b1bb41e1231fdc30620c38e33bba
Matthew Wimmer <donmateo@gmail.com>
2014-10-23 13:24:39 -0400

    Move tab-bar to partial like top-bar and off-canvas

--- header.php
+++ header.php
@@ -16,19 +16,10 @@
 	
 	<div class="off-canvas-wrap" data-offcanvas>
 	<div class="inner-wrap">
-	
-	<?php do_action('foundationPress_layout_start'); ?>
-	
-	<nav class="tab-bar show-for-small-only">
-		<section class="left-small">
-			<a class="left-off-canvas-toggle menu-icon" href="#"><span></span></a>
-		</section>
-		<section class="middle tab-bar-section">
-			
-			<h1 class="title"><?php bloginfo( 'name' ); ?></h1>
 
-		</section>
-	</nav>
+	<?php do_action('foundationPress_layout_start'); ?>
+
+	<?php get_template_part('parts/tab-bar'); ?>
 
 	<?php get_template_part('parts/off-canvas-menu'); ?>
 
---
+++ parts/tab-bar.php
@@ -0,0 +1,10 @@
+<nav class="tab-bar show-for-small-only">
+	<section class="left-small">
+		<a class="left-off-canvas-toggle menu-icon" href="#"><span></span></a>
+	</section>
+	<section class="middle tab-bar-section">
+
+		<h1 class="title"><?php bloginfo( 'name' ); ?></h1>
+
+	</section>
+</nav>
