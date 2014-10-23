825e603e92d0f58d853e571d188a1eb35bd53ca3
Matthew Wimmer <donmateo@gmail.com>
2014-10-23 12:35:19 -0400

    Move title tag functions and app/favicons to separate included library to make header.php cleaner

---
+++ library/head-section.php
@@ -0,0 +1,41 @@
+<?php
+if (!function_exists('FoundationPress_title_tag')) :
+	function FoundationPress_title_tag() {
+		if ( is_category() ) {
+			echo 'Category Archive for &quot;'; single_cat_title(); echo '&quot; | '; bloginfo( 'name' );
+		} elseif ( is_tag() ) {
+			echo 'Tag Archive for &quot;'; single_tag_title(); echo '&quot; | '; bloginfo( 'name' );
+		} elseif ( is_archive() ) {
+			wp_title(''); echo ' Archive | '; bloginfo( 'name' );
+		} elseif ( is_search() ) {
+			echo 'Search for &quot;'.esc_html($s).'&quot; | '; bloginfo( 'name' );
+		} elseif ( is_home() || is_front_page() ) {
+			bloginfo( 'name' ); echo ' | '; bloginfo( 'description' );
+		}  elseif ( is_404() ) {
+			echo 'Error 404 Not Found | '; bloginfo( 'name' );
+		} elseif ( is_single() ) {
+			wp_title('');
+		} else {
+			echo wp_title( ' | ', 'false', 'right' ); bloginfo( 'name' );
+		}
+	}
+endif;
+
+if (!function_exists('FoundationPress_icon_links')) :
+	function FoundationPress_icon_links() {
+		$directory = get_stylesheet_directory_uri() . "/assets/img/icons/";
+		$icon_sizes = array(
+				'large'		=>	'144x144',
+				'medium'	=>	'114x114',
+				'small'		=>	'72x72',
+				'none'		=>	''
+			);
+		echo '<link rel="icon" href="' . $directory . 'favicon.ico" type="image/x-icon">';
+		foreach ($icon_sizes as $size) {
+			$link = '<link rel="apple-touch-icon-precomposed" sizes="' . $size;
+			$link .= '" href="' . $directory . 'apple-touch-icon-' . $size . '-precomposed.png">';
+			echo $link;
+		}
+	}
+endif;
+?>
